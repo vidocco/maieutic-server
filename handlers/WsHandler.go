dd0e7b93bdab9e55015f0fe7dfd67231b7cb0cc0
Jack Thibaut <jack.mk.thibaut@gmail.com>
2018-11-18 21:11:47 +0100

    feat: ws handshake completed

--- handlers/WsHandler.go
+++ handlers/WsHandler.go
@@ -1,14 +1,48 @@
 package handlers
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/naoina/denco"
+	"log"
+	"maieutic-server/env"
+	"maieutic-server/sockets"
+	"maieutic-server/utils"
 	"net/http"
+	"strconv"
 )
 
-type msg struct {
-	Type string
+var rBuffSize, _ = strconv.Atoi(env.GetOr("R_BUFF_SIZE", "1024"))
+var wBuffSize, _ = strconv.Atoi(env.GetOr("W_BUFF_SIZE", "1024"))
+
+func origCheck (r *http.Request) bool {
+	if env.GetOr("GO_ENV", "development") == "development" {
+		return true
+	}
+	return false
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  rBuffSize,
+	WriteBufferSize: wBuffSize,
+	CheckOrigin: origCheck,
 }
 
 func WsHandler (w http.ResponseWriter, r *http.Request, params denco.Params) {
-	w.WriteHeader(http.StatusOK)
+	conn, err := upgrader.Upgrade(w, r, nil)
+	utils.CheckErr(err)
+	log.Print("heyo", conn)
+
+	defer conn.Close()
+	for {
+		var msg sockets.Event
+		err := conn.ReadJSON(&msg)
+
+		if err != nil {
+			log.Printf("error: %v", err)
+			sockets.Disconnect(conn)
+			break
+		}
+
+		sockets.Broadcast <- msg
+	}
 }
